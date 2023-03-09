93e5ad4fa3afb3e05eee2c901c1ad20089a84b4b
ErikFA <erikfial1@gmail.com>
2023-03-09 19:48:58 +0100

    Update exercise_24.py

--- lessons/classes/exercises/exercise_24.py
+++ lessons/classes/exercises/exercise_24.py
@@ -2,50 +2,32 @@ class Coordinate:
     def __init__(self,x,y):
         self.x=x
         self.y=y
-@property
-def x(self):
-    return float(self.x)
-@property
-def y(self):
-    return float(self.y)
-@x.setter
-def x(self,x):
-    if isistance(x,int) or isistance(x,float):
+    @property
+    def x(self):
+        return float(self._x)
+    @property
+    def y(self):
+        return float(self._y)
+    @x.setter
+    def x(self,x):
+        if not isinstance(x, (int, float)):
+            raise TypeError("the value must be a int/float")
         self._x=x
-    else:
-        raise TypeError("the value must be a int/float")
-@y.setter
-def y(self,y):
-    if isistance(y,int) or isistance(y,float):
-        self._y=y
-    else:
-        raise TypeError("the value must be a int/float")
-def __add__(self,other):
-    return Coordinate(self.x+other.x,self.y+other.y)
-def __sub__(self,other):
-    return Coordinate(self.x-other.x,self.x-self.y)
-def __len__(self):
-    return int((self.x**2+self.y**2)**(1/2))
-def __hash__(self):
-    return int(len(self))
-def __iter__(self):
-    iter((self.x,self.y))
-def __getattr__(self,attr):#Más eficiente diccionario pero yo paso de implementarlo,bueno eficiente no,más bonito
-    if attr=="x" or attr=="_x":
-        toret=self._x
-    elif attr=="y" or attr=="_y":
-        toret=self._y
-    else:
-        raise TypeError("the value must be consider as a atributte")
-    return toret
-def __setattr__(self,attr,mod):
-    if attr=="x" or attr=="_x":
-        self._x=x
-    elif attr=="y" or attr=="_y":
-        self._y=y
-    else:
-        raise TypeError("the value must be consider as a atributte")
-def __repr__(self):#Deberíamos usar un StringBuilder
-    return "("+str(self.x)+","+str(self.y)+")"
-def __str__(self):#/Deberíamos usar un StringBuilder
-    return "X="+str(self.x)+"\tY="+str(self)
+    @y.setter
+    def y(self,y):
+        if isinstance(y,int) or isinstance(y,float):
+            self._y=y
+        else:
+            raise TypeError("the value must be a int/float")
+    def __add__(self,other):
+        return Coordinate(self.x+other.x,self.y+other.y)
+    def __sub__(self,other):
+        return Coordinate(self.x-other.x,self.y-other.y)
+    def __len__(self):
+        return int((self.x**2+self.y**2)**(1/2))
+    def __repr__(self):#Deberíamos usar un StringBuilder
+        return "("+str(self.x)+","+str(self.y)+")"
+    def __str__(self):#/Deberíamos usar un StringBuilder
+        return "X="+str(self.x)+"\tY="+str(self)
+    def distance_to_origin(self):#Se puede hacer más eficiente pero es una tonteria
+        return ((self.x)**2+(self.y)**2)**(1/2)
